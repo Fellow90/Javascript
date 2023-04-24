738e0106ff91b3b4b7502fc5f12ced2ed290266a
Fellow90 <119867781+Fellow90@users.noreply.github.com>
2023-04-24 16:48:09 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = "Harish";
+let age = 25;
+let student = true;
+
+console.log('Hello ' +name);
+console.log('You age is ',age);
+console.log('Enrollment ',student);
+
+document.getElementById('p1').innerHTML = 'Hello ' + name;
+document.getElementById('p2').innerHTML = 'You age is '+age;
+document.getElementById('p3').innerHTML = 'Enrollment '+student;
+
+
+
